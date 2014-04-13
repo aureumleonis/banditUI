d0a94e71d98d95e20278a0bee0e2467793343732
U-Daniel-HP\Daniel <Daniel@Daniel-HP.(none)>
2014-04-13 14:07:39 -0500

    Continued work on Person, Context classes.

---
+++ banditUI/src/articleRecommendationSystem/BanditSimulator/Main.java
@@ -0,0 +1,14 @@
+package system;
+
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Person p1 = new Person(false);
+        Context c1 = new Context();
+    }
+
+}
+
