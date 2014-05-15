bb94ea1ead8d1df8eb61cc347194b4130250264a
dbaxter240 <baxte141@umn.edu>
2014-05-14 21:32:31 -0500

    Fixed bugs with emphasis value func in Expert.

--- banditUI/src/com/banditUI/articleViewer/system/Expert.java
+++ banditUI/src/com/banditUI/articleViewer/system/Expert.java
@@ -10,6 +10,8 @@ public class Expert {
 	
 	public Expert() {
 		context = new Context();
+		beta = Math.random() * 0.8;
+		alpha = Math.random() * beta;
 	}
 	
 	// Input articlesAvailable is a hashmap from articles to how many squares in the
@@ -21,7 +23,7 @@ public class Expert {
 		HashMap<Article, Double> articleVals = new HashMap<Article, Double>();
 		for (Article a : articles.keySet()){
 			topic = a.getTopic();
-			click_prob = ClickProbCalc.calcClickProb(a, context.getFeatureVector(topic));
+			click_prob = ClickProbCalc.calcClickProb(a);
 			// Apply the emphasis value function the necessary number of times.
 			num_spaces_taken = articles.get(a);
 			click_prob = ApplyEmphasisValueFunction(click_prob, num_spaces_taken);
@@ -40,7 +42,7 @@ public class Expert {
 	private double ApplyEmphasisValueFunction(double input, int num_times) {
 		double val = input;
 		for (int i = 0; i < num_times; i++) {
-			val = Math.min(alpha*val, beta*(1-val));
+			val += Math.min(alpha*val, beta*(1-val));
 		}
 		return val;
 	}
